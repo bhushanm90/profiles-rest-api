15ea98e9516197bb0aa25401c1d4d3c6b83057b5
Bhushan <mahajan.bhushan1990@gmail.com>
2025-02-04 11:58:56 +0530

    enabled django admin for user profile

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -21,13 +21,13 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """ Create and save a new super user with given details"""
         user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
-        user.save(using.self._db)
+        user.save(using=self._db)
 
         return user
 
@@ -46,6 +46,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     def get_full_name(self):
         """ Retrive full name of user"""
         return self.name
+
     def get_short_name(self):
         """ Retrive short name of user"""
         return self.name
